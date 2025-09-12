7bb31e0896b181daf15e0f28b23a3901b52af0ef
Paige Li <LIPA3@oocl.com>
2025-09-12 14:46:36 +0800

    feat: rendering todo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import {createContext, useContext, useReducer} from "react";
+
+export const initState = [
+  {id: 1, text: "the first todo", done: false},
+  {id: 2, text: "the second todo", done: false},
+];
+export const TodoContext = createContext()
+
+function TodoItem(props) {
+  return <div>{props.todo.text}</div>;
+}
+
+function TodoGroup() {
+  const {state,dispatch}=useContext(TodoContext);
+  return <div>
+    {
+      state.map((item,index) => {
+        return <TodoItem todo={item} key={index} index={index}></TodoItem>
+      })
+    }
+  </div>;
+}
+
+export function todoReducer(state, action) {
+  return state
+}
 
 function App() {
+  const [state, dispatch] = useReducer(todoReducer, initState);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <TodoContext.Provider value={{state, dispatch}}>
+          <TodoGroup/>
+        </TodoContext.Provider>
+      </div>
   );
 }
 
