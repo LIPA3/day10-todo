780b006247a8e4ec73ad0ae4f0c993be14546a0b
Paige Li <LIPA3@oocl.com>
2025-09-15 11:23:42 +0800

    refactor: refactor pages

--- src/App.js
+++ src/App.js
@@ -2,52 +2,10 @@ import {useReducer} from "react";
 import "./App.css"
 import {todoReducer} from "./reduces/TodoReducer";
 import {TodoContext} from "./contexts/TodoContext";
-import TodoList from "./components/TodoList";
-import {Outlet,NavLink,createBrowserRouter} from "react-router";
-import {useRouteError} from "react-router";
+import { RouterProvider} from "react-router";
 // import {} from "react-router-dom";
+import route from "./routes/Routes";
 
-const route=createBrowserRouter([
-    {
-        path:'/',
-        element:<DefaultLayout></DefaultLayout>,
-        errorElement:<ErrorPage></ErrorPage>,
-        children:[
-            {
-                path:'/',
-                element:<HomePage></HomePage>
-            }
-        ]
-    }
-])
-function HomePage(){
-    return <div>
-        <TodoList></TodoList>
-    </div>
-}
-function ErrorPage(){
-    const error=useRouteError();
-    return <div>
-        {JSON.stringify(error)}
-        {error.status === 404
-         ? <div>Page not found<span>try</span></div>
-         : <div>{JSON.stringify(error)}</div>}
-    </div>
-}
-function DefaultLayout(){
-    return <div>
-        <header>
-            <nav>
-                <ul>
-                    <li><NavLink to={"/"}>Home</NavLink></li>
-                </ul>
-            </nav>
-        </header>
-        <main>
-            <Outlet></Outlet>
-        </main>
-    </div>
-}
 export const initState = [
     {id: 1, text: "the first todo", done: false},
     {id: 2, text: "the second todo", done: true},
@@ -58,7 +16,7 @@ function App() {
         <div className="App">
             <h2 className={"title"}>Todo List</h2>
             <TodoContext.Provider value={{state, dispatch}}>
-                <TodoList></TodoList>
+               <RouterProvider router={route}></RouterProvider>
             </TodoContext.Provider>
         </div>
     );
