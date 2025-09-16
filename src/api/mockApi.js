7e6f088298b6a607a99535aff62a31e60804499c
Paige Li <LIPA3@oocl.com>
2025-09-16 19:19:37 +0800

    feat:add interceptors

--- src/api/mockApi.js
+++ src/api/mockApi.js
@@ -1,10 +1,29 @@
+// import { message } from "antd";
+import { message } from "antd";
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://68c7acbf5d8d9f5147328947.mockapi.io/",
+    // baseURL: "https://68c7acbf5d8d9f5147328947.mockapi.io/",
+    baseURL: "http://localhost:8080/",
     headers:{
         "Content-Type":"application/json",
     },
     timeout:10000
 });
+api.interceptors.response.use(
+   (response) => {
+     // handle response
+     return response;
+   },
+   (error) => {
+     // handle response error
+	const {status, data} = error.response;
+    console.log(status, data);
+    
+      if (status === 404) {
+        message.error(error.message,10000);
+      }
+     return Promise.reject(error);
+   }
+);
 export default api;
